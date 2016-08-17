77d4f38169bc02f8d1c2affe51167582b0ca8d36
1983164263@qq.com <1983164263@qq.com>
2016-08-17 16:21:36 +0800

    增加新功能

---
+++ app/src/main/java/com/joy/mygithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.joy.mygithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
